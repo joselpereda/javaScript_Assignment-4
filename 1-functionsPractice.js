c5de414d1903e27e89765358c239a225b057ff49
joselpereda <joselpereda@hotmail.com>
2023-03-02 12:33:25 -0800

    corrected math method in line 9

--- 1-functionsPractice.js
+++ 1-functionsPractice.js
@@ -1,23 +1,23 @@
 //STEP 1
 function halfNumber(numToHalf) {
-     return `Half of ${numToHalf} is ${numToHalf/2}.`;
+    return `Half of ${numToHalf} is ${numToHalf/2}.`;
 }
-// console.log(halfNumber(97));
+console.log(halfNumber(97));
 
 //STEP 2
 function squareNumber(numToSquare) {
-     return `The result of squaring the number ${numToSquare} is ${math.pow(numToSquare,2)}.`;
+    return `The result of squaring the number ${numToSquare} is ${Math.pow(numToSquare,2)}.`;
 }
-// console.log(squareNumber(7));
+console.log(squareNumber(7));
 
 // //STEP 3
 function percentOf(num1, num2) {
-     return `${num1} is ${num1/num2 * 100}% of ${num2}.`;
+    return `${num1} is ${num1/num2 * 100}% of ${num2}.`;
 }
 // console.log(percentOf(25,100));
 
 //STEP 4
 function findModulus(x, y) {
-     return `${x % y} is the modules of ${x} and ${y}.`
+    return `${x % y} is the modules of ${x} and ${y}.`
 }
-// console.log(findModulus(7,3));
+console.log(findModulus(7,3));
