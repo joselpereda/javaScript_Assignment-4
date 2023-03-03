eeca71623b5643271ae809c8f45ec0fe58182b98
joselpereda <joselpereda@hotmail.com>
2023-03-03 02:00:48 -0800

    added calculator.js

---
+++ js/modules/calculator.js
@@ -0,0 +1,31 @@
+// ADD A PRIVATE FUNCTION CALLED CALCULATE
+(function calculate() {
+    'use strict';
+}) ();
+
+// ADD FOUR PUBLIC FUNCTIONS BELOW
+// ADD FUNCTION
+const add = (x,y) => {
+    return x + y;
+}
+
+// SUBTRACT FUNCTION
+const subtraction = (x,y) => {
+    return x - y;
+}
+
+// MULTIPLY FUNCTION
+const multiply = (x,y) => {
+    return x * y;
+}
+
+// DIVIDE FUNCTION
+const divide = (x,y) => {
+    return x / y;
+}
+ 
+// EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add};
+export {subtraction};
+export {multiply};
+export {divide};
