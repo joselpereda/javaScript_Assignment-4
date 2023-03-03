d35a465de131d73a932990b1ab9c3aec06e9b71b
joselpereda <joselpereda@hotmail.com>
2023-03-03 02:21:44 -0800

    added <script type="module" src="js/3-basiccalculator.js"></script> to line 10

--- js/3-basicCalculator.js
+++ js/3-basicCalculator.js
@@ -5,15 +5,15 @@ let operandChoice;
 let operandChoiceUpper;
 
 // IMPORT THE MODULE
-import * as calc from "./modules/calculator.js";
+*import * as calc from "./modules/calculator.js"; 
 
 // Prompt user for first number
 do {
     number1 = Number(prompt("Enter the first number:"));
-} while (typeof(number1) === "number")
+} while (typeof(number1) !== "number")
 
 // Prompt user for second number
-do {
+/*do {
     number2 = Number(prompt("Enter the second number:"));
 } while (typeof(number2) === "number")
 
@@ -32,7 +32,7 @@ do {
     } else
         break;
 } while (true);
-
+*/
 //evaluate the operation parameter being passed and perform the appropriate operation
 switch (operandChoiceUpper) {
     case "ADD":
