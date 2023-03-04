efe4f3f40f53ca97e56244e893c114660beb0c1e
joselpereda <joselpereda@hotmail.com>
2023-03-04 09:07:14 -0800

    multiple changes

--- js/3-basicCalculator.js
+++ js/3-basicCalculator.js
@@ -5,12 +5,12 @@ let operandChoice;
 let operandChoiceUpper;
 
 // IMPORT THE MODULE
-*import * as calc from "./modules/calculator.js"; 
+import * as calc from "./modules/calculator.js"; 
 
 // Prompt user for first number
-do {
+//do {
     number1 = Number(prompt("Enter the first number:"));
-} while (typeof(number1) !== "number")
+//} while (typeof(number1) !== "number")
 
 // Prompt user for second number
 /*do {
@@ -34,7 +34,7 @@ do {
 } while (true);
 */
 //evaluate the operation parameter being passed and perform the appropriate operation
-switch (operandChoiceUpper) {
+/*switch (operandChoiceUpper) {
     case "ADD":
         alert(`${number1} + ${number2} = ${calc.add(number1, number2)}`);
         break;
@@ -50,4 +50,4 @@ switch (operandChoiceUpper) {
     default:
         alert("No match!")
         break;                 
-}
+} */
