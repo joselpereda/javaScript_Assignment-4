8f83fc7f04ef5e09c0099b7673b9c73fb8b1488e
joselpereda <joselpereda@hotmail.com>
2023-03-06 20:58:57 -0800

    logic changes

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,27 +1,27 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
-(function calculate() {
+/*(function calculate() {
     'use strict';
-}) ();
+}) (); */
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 // ADD FUNCTION
 const add = (x,y) => {
-    return x + y;
+    return x + y
 }
 
 // SUBTRACT FUNCTION
 const subtraction = (x,y) => {
-    return x - y;
+    return x - y
 }
 
 // MULTIPLY FUNCTION
 const multiply = (x,y) => {
-    return x * y;
+    return x * y
 }
 
 // DIVIDE FUNCTION
 const divide = (x,y) => {
-    return x / y;
+    return x / y
 }
  
 // EXPORT THE FOUR PUBLIC FUNCTIONS
